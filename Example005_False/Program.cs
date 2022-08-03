e2610033c7ae5f8a4d1bfc779c3d847e3efb695f
Veronika <valeria14081981@yandex.com>
2022-08-03 23:49:44 +0300

    This is MaShA!

---
+++ Example005_False/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Input name user: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("URA, this is the Masha!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
