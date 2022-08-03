deb24b6b92e44f830b70999194e2b1f1f9c9ea8a
Veronika <valeria14081981@yandex.com>
2022-08-04 02:07:20 +0300

    Tochki

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -2,9 +2,9 @@
 //Console.SetCursorPosition(10, 4);
 //Console.WriteLine("+");
 
-int xa = 40, ya = 1,
-    xb = 1, yb = 30,
-    xc = 60, yc = 30;
+int xa = 40, ya = 1;
+int xb = 1, yb = 30;
+int xc = 60, yc = 30;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
