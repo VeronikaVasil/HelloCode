30503ef7b78a4708455b107633649c1f5ea267d1
Veronika <valeria14081981@yandex.com>
2022-08-14 01:07:45 +0300

    Find index

---
+++ Example010_Method/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {1, 12, 31, 4, 15, 1, 17, 19};
+
+int n = array.Length;
+int find = 1;
+
+int index = 0;
+
+while (index < n)
+{
+   if(array[index] == find) 
+   {
+    Console.WriteLine(index);
+    break;
+   }
+   
+   // index = index + 1;
+    index++;
+}
