447bfcbfa9ba89c4acf53e0cdd43e9ae876abe4c
Veronika <valeria14081981@yandex.com>
2022-08-13 15:31:33 +0300

    Method2

---
+++ Method2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Вид2
+// Методы, которые ничего не возвращают, но могут принимать какие-то аргументы
+
+void Method2(string msg)
+{
+   Console.WriteLine(msg); 
+}
+
+Method2("Message Text");
+
