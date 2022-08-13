6bc6874179fd682bf118410f7c38311c005870cc
Veronika <valeria14081981@yandex.com>
2022-08-13 16:22:33 +0300

    Method 3

---
+++ Method3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Вид3
+//Обязателен оператор return
+// Метод, который что-то возвращает, но ничего не принимает
+
+int Method3()
+
+{
+    return DateTime.Now.Year; //в теле метода произошла какая-то работа
+}
+
+int year = Method3();
+Console.WriteLine(year);
+
