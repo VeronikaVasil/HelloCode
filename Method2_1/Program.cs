97ca6de19dd6f88cbac1aa48fea643b26f31c717
Veronika <valeria14081981@yandex.com>
2022-08-13 16:02:20 +0300

    Method2.1

---
+++ Method2_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+void Method21(string msg, int count)
+
+{
+
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+
+}
+
+Method21("Text", 4);
+
+
