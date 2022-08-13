054fb42f1b403ac6b98d03608e0e20016e7b553a
Veronika <valeria14081981@yandex.com>
2022-08-13 16:11:21 +0300

    Method2_2

---
+++ Method2_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// 2.1
+
+void Method21(string msg, int count)
+
+{
+
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+
+}
+
+Method21(msg:"Text", count: 4); //указываем какому аргументу какое значение хотим присвоить
