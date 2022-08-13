8d1af6753fee869fa71334adfc4bcf7c526dac32
Veronika <valeria14081981@yandex.com>
2022-08-13 19:02:25 +0300

    Замена больших букв В маленькими буквами в

---
+++ Text_work/Program.cs
@@ -0,0 +1,40 @@
+﻿// ====Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить чёрточками.
+// Маленькие буквы "к" заменить большими "К"
+// Большие буквы "С" заменить маленькими "с"
+
+//Ясна ли задача?
+
+string text = "- я думаю, - сказал князь, улыбаясь, - что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы знали приступом согласие прусского короля."
+            + "Вы так красноречивы. Выдадите мне чаю?";
+// string s = "qwerty"
+//             012345
+// s[3]  // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
+
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+
+Console.WriteLine();
+newText = Replace(newText, 'В', 'в');
+Console.WriteLine(newText);
