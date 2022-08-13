52848809c69b09ac91bd487a7a00f5662b9cf7dd
Veronika <valeria14081981@yandex.com>
2022-08-13 17:11:27 +0300

    Method

---
+++ Method4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Вид4
+// Программы что-то принимают и что-то возвращают
+
+string Method4(int count, string text)
+
+{
+   int i = 0;
+   string result = String.Empty;
+        while (i < count)
+        {
+            result = result + text;
+            i++;
+        }
+        return result;
+
+}
+
+string res = Method4(10, "asdf");
+Console.WriteLine(res);
+
+
