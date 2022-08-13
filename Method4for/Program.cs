d45ced7a3dee754106c4d427506ff563133c6cd7
Veronika <valeria14081981@yandex.com>
2022-08-13 17:44:05 +0300

    Method4for

---
+++ Method4for/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программы что-то принимают и что-то возвращают. Цикл for
+
+string Method4(int count, string text)
+
+{
+
+   string result = String.Empty;
+        for (int i = 0; i < count; i++)
+        
+        {
+            result = result + text;
+        
+        }
+        return result;
+
+}
+
+string res = Method4(10, "a");
+Console.WriteLine(res);
+
