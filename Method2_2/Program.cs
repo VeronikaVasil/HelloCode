b5f130cf77026312aa6cd18a959d7df927b9488c
Veronika <valeria14081981@yandex.ru>
2022-10-21 10:17:36 +0300

    write text 4 times

--- Method2_2/Program.cs
+++ Method2_2/Program.cs
@@ -1,16 +1,11 @@
 ﻿// 2.1
-
 void Method21(string msg, int count)
-
 {
-
     int i = 0;
     while (i < count)
     {
         Console.WriteLine(msg);
         i++;
     }
-
 }
-
-Method21(msg:"Text", count: 4); //указываем какому аргументу какое значение хотим присвоить
+Method21(msg: "Text", count: 4); //указываем какому аргументу какое значение хотим присвоить
